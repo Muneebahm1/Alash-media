6c8f00a705c5218c540cc27ca01d8209fd90d9e8
Muneebahm1 <98212039+Muneebahm1@users.noreply.github.com>
2025-10-01 21:53:04 +0500

    fixes 2.2

--- app/[locale]/authors/page.tsx
+++ app/[locale]/authors/page.tsx
@@ -9,23 +9,19 @@ export default async function page(
     page: string;
     pathName?: string;
 
-    title?: string;
   
   }>;}
 ) {
   
     const searchParams = await props.searchParams;
-    const authorQuery = searchParams?.searchquery || '';
-    const searchAlpha = searchParams?.searchLetter || '';
-
-    const tquery = searchParams?.searchquery || '';
-    //const authorCurrentPage = Number(searchParams?.page) || 1;
+  const authorQuery = searchParams?.searchquery || '';
+  const searchAlpha = searchParams?.searchLetter || '';
+  //const authorCurrentPage = Number(searchParams?.page) || 1;
                   
     return (
       <main className="mt-1.5">
         <section className='mx-auto md:mx-[120px] px-4 pt-0.5 pb-0.5 '>
           <div className="bg-[#E9FFE5] px-5">
-            <h1 className='text-3xl font-bold md:p-5'>Authors</h1>
             <div className="p-4 md:px-5 w-full md:w-96 md:p-5">
               <Search />
             </div>
